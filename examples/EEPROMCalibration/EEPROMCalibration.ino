eddb6ad0130d7dcb1f5d8a7f3ad3eb47f68df9b4
Anthony Hernandez <anthony.hernandez@clownphobia.com>
2018-04-23 13:19:25 -0500

    Revert "Fixes issue #8"
    This reverts commit 90f0a159c57265962d41289cd89a6647ba2fe000.

--- examples/EEPROMCalibration/EEPROMCalibration.ino
+++ examples/EEPROMCalibration/EEPROMCalibration.ino
@@ -19,9 +19,9 @@ uint32_t load(uint16_t address) {
 		Serial.println("No calibration value stored into EEPROM, using default");
 		calibration = DEFAULT_REFERENCE_CALIBRATION;
 	} else {
-		Serial.print(F("Read from EEPROM address "));
+		Serial.print("Read from EEPROM address ");
 		Serial.print(address);
-		Serial.print(F(" calibration value "));
+		Serial.print(" calibration value ");
 		Serial.println(calibration);
 	}
 	return calibration;
@@ -34,7 +34,7 @@ void save(uint16_t address, uint32_t calibration) {
 	EEPROM.write(address, calibration >> 16);
 	EEPROM.write(address + 1, calibration >> 8);
 	EEPROM.write(address + 2, calibration & 0xFF);
-	Serial.println(F("Saved calibration value into EEPROM"));
+	Serial.println("Saved calibration value into EEPROM");
 }
 
 /**
@@ -44,19 +44,19 @@ void clear(uint16_t address) {
 	EEPROM.write(address, 0xFF);
 	EEPROM.write(address + 1, 0xFF);
 	EEPROM.write(address + 2, 0xFF);
-	Serial.println(F("Cleared calibration value from EEPROM"));
+	Serial.println("Cleared calibration value from EEPROM");
 }
 
 void menu() {
-	Serial.println(F("--- MENU ---"));
-	Serial.println(F("\tR to read Vcc"));
-	Serial.println(F("\tL to load calibration from EEPROM"));
-	Serial.println(F("\tS to store calibration into EEPROM"));
-	Serial.println(F("\tC to clear calibration from EEPROM"));
-	Serial.println(F("\tdddd (4 digits) to calibrate for mV"));
-	Serial.println(F("\tA to print EEPROM calibration start address (length 3)"));
-	Serial.println(F("\tAdddd (4 digits) to set EEPROM calibration start address (length 3)"));
-	Serial.println(F("\tH prints this help"));
+	Serial.println("--- MENU ---");
+	Serial.println("\tR to read Vcc");
+	Serial.println("\tL to load calibration from EEPROM");
+	Serial.println("\tS to store calibration into EEPROM");
+	Serial.println("\tC to clear calibration from EEPROM");
+	Serial.println("\tdddd (4 digits) to calibrate for mV");
+	Serial.println("\tA to print EEPROM calibration start address (length 3)");
+	Serial.println("\tAdddd (4 digits) to set EEPROM calibration start address (length 3)");
+	Serial.println("\tH prints this help");
 }
 
 VoltageReference vRef;
@@ -78,12 +78,12 @@ void parse() {
 		} else if (c == 'l' || c == 'L') {
 			vRef.begin(load(address));
 		} else if (c == 'r' || c == 'R') {
-			Serial.print(F("Calculated input voltage is "));
+			Serial.print("Calculated input voltage is ");
 			Serial.print(vRef.readVcc());
-			Serial.println(F("mV"));
-			Serial.print(F("Real bandgap voltage is "));
+			Serial.println("mV");
+			Serial.print("Real bandgap voltage is ");
 			Serial.print(vRef.internalValue());
-			Serial.println(F("mV"));
+			Serial.println("mV");
 		}  else if (c == 'a' || c == 'A') {
 			if (Serial.available() > 0) {
 				c = Serial.read();
@@ -95,16 +95,16 @@ void parse() {
 				}
 				buffer[i++] = '\0';
 				address = atol(buffer);
-				Serial.print(F("Setting EEPROM start address to "));
+				Serial.print("Setting EEPROM start address to ");
 				Serial.println(address);
 			} else {
-				Serial.print(F("EEPROM start address is "));
+				Serial.print("EEPROM start address is ");
 				Serial.println(address);
 			}
 		} else if (c == 'h' || c == 'H') {
 			menu();
 		}
-	} else if(isdigit(c)){
+	} else if (isdigit(c)) {
 		char* buffer = new char[5];
 		uint8_t i = 0;
 		while (isdigit(c)) {
@@ -113,15 +113,15 @@ void parse() {
 		}
 		buffer[i++] = '\0';
 		long voltage = atol(buffer);
-		Serial.print(F("Calibrating for Vcc "));
+		Serial.print("Calibrating for Vcc ");
 		Serial.print(voltage);
 		Serial.println("mV");
 		uint32_t calibration = vRef.calibrate(voltage);
-		Serial.print(F("Calibration value is "));
+		Serial.print("Calibration value is ");
 		Serial.println(calibration);
-		Serial.print(F("Real bandgap voltage is "));
+		Serial.print("Real bandgap voltage is ");
 		Serial.print((calibration + (ANALOG_MAX_VALUE / 2)) / ANALOG_MAX_VALUE);
-		Serial.println(F("mV"));
+		Serial.println("mV");
 		vRef.begin(calibration);
 	}
 }
